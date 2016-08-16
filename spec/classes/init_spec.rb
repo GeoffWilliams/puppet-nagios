febf9dfbe53215f9ee1b1dfa5b4c8f86f0a41b04
Geoff Williams <geoff.williams@puppetlabs.com>
2016-08-16 13:56:30 +1000

    unit tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 describe 'nagios' do
+  let :facts do
+    {
+      :id                     => 'root',
+      :kernel                 => 'Linux',
+      :lsbdistcodename        => 'squeeze',
+      :osfamily               => 'RedHat',
+      :operatingsystem        => 'centos',
+      :operatingsystemrelease => '7',
+      :path                   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      :concat_basedir         => '/dne',
+      :is_pe                  => false,
+    }
+  end
+
   context 'with default values for all parameters' do
     it { should contain_class('nagios') }
   end
